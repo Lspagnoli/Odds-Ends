34917b5477df324288bb5ce10ac9df36ef05619a
Luke Spagnoli <spagnoli.luke@gmail.com>
2023-11-06 13:57:44 -0500

    Update csvCombiner.py

--- csvCombiner.py
+++ csvCombiner.py
@@ -2,34 +2,49 @@ import os
 import glob
 import pandas as pd
 
-def merge():
+def merge(dataPath, outputPath):
 
-    path = "/Users/lspagnoli/Desktop/data"
-
-    all_files = glob.glob(os.path.join(path, "*.csv"))
+    print("Starting Merge...")
+    
+    all_files = glob.glob(os.path.join(dataPath, "*.csv"))
     df = pd.concat((pd.read_csv(f) for f in all_files))
-
-    df.to_csv("/Users/lspagnoli/Desktop/NEWMerged.csv", index=False)
-
+    df.to_csv(outputPath, index=False)
+    
     print("Merged!")
 
-def toCSV():
+def dtaToCSV(toConvertPath, outputPath):
 
-    #dataNew = pd.read_stata('/Users/lspagnoli/Desktop/dataNew.dta')
-    dataOld = pd.read_stata('/Users/lspagnoli/Desktop/dataOld')
-
-    #dataNew.to_csv('/Users/lspagnoli/Desktop/data/new.csv', index=False)
-    dataOld.to_csv('/Users/lspagnoli/Desktop/dataNew', index=False)
+    print("Starting Conversion...")
+    
+    dataOld = pd.read_stata(toConvertPath)
+    dataOld.to_csv(outputPath, index=False)
 
     print("Converted!")
 
 def main():
+    #Uncomment the helper functions you require!
+    
+   #~~~~~~~~~~~~dtaToCSV~~~~~~~~~~~~
+    #converts dta files to csv files:
 
-    #print("Starting Conversion...")
-    #toCSV()
-    print("Starting Merge...")
-    merge()
-    print("Complete!!!")
+    """
+    toConvertPath = '/Users/lspagnoli/Desktop/dataOld' #file path for the dta file to be converted
+    outputPath = '/Users/lspagnoli/Desktop/dataNew' #the location of the converted file, including new file name
+    
+    dtaToCSV(toConvertPath, outputPath)
+    """
+    
+    #~~~~~~~~~~~~merge~~~~~~~~~~~~
+    #takes all csv files in a folder and merges them into one new file:
+
+    """
+    toConvertPath = '/Users/lspagnoli/Desktop/data' #file path for the folder containing data to be merged
+    outputPath = "/Users/lspagnoli/Desktop/NEWMerged.csv" #the location of the new merged file, including new file name
+    
+    merge(toConvertPath, outputPath)
+    """
+
+     print("Complete :3")
 
 
 main()
